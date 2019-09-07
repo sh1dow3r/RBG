630ca85e793f72add2b40754ddf8046975027f88
sh1dow3r <sh1dow3r@gmail.com>
2019-09-07 13:36:01 -0400

    updating gitlab firewall ruels

--- Blue/GitLab_Firewall.sh
+++ Blue/GitLab_Firewall.sh
@@ -8,7 +8,7 @@
 SERVER_IP="$(ip addr show eth0 | grep 'inet ' | cut -f2 | awk '{ print $2}')"
 
 #alias for less typeing 
-ip=iptaples 
+ip=iptables 
 
 #Flush All the tables(default "F" and non-default chain "X") to make sure only our firewall rule existed
 $ip -F
@@ -18,15 +18,20 @@ $ip -t nat -X
 $ip -t mangle -F
 $ip -t mangle -X
 
-#Allow ICMP protocol for debugging usage
-$ip -A INPUT -p ICMP --icmp-type echo-request -h ACCEPT
-$ip -A OUTPUT -P ICMP --icmp-type echo-replay -j ACCEPT
+#Allow ICMP protocol out for debugging usage
+#ICMP-TYPE 8 is Echo, and 0 is Echo-Replay
+$ip -A INPUT -p ICMP --icmp-type 8 -j ACCEPT
+$ip -A OUTPUT -p ICMP --icmp-type 0 -j ACCEPT
+
+#Allow ICMP protocol in for debugging usage 
+#$ip -A OUTPUT -p ICMP --icmp-type 8 -j ACCEPT
+#$ip -A INPUT -p ICMP --icmp-type 0 -j ACCEPT
 
 #Allow DNS quireies 
 $ip -A OUTPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
 $ip -A INPUT  -p udp --sport 53 -m state --state ESTABLISHED     -j ACCEPT
-iptables -A OUTPUT -p tcp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT  -p tcp --sport 53 -m state --state ESTABLISHED     -j ACCEPT
+$ip -A OUTPUT -p tcp --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+$ip -A INPUT  -p tcp --sport 53 -m state --state ESTABLISHED     -j ACCEPT
 
 # Allow unlimited traffic on loopback
 iptables -A INPUT -i lo -j ACCEPT
@@ -40,7 +45,7 @@ iptables -A OUTPUT -p tcp -m multiport --sports 22,80,443,9418 -m state --state 
 
 #Deafult drop 
 $ip -A INPUT -j DROP
-$ip -A OUTPUT -J DROP
+$ip -A OUTPUT -j DROP
 
 #FOR TESTING PURPOSES
 #SLEEP 5 && $ip -F &
